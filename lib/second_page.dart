070ac6a2dc5e3e150f52e09b3ca3466c69090a76
areesha-m <areeshamehmood1@gmail.com>
2024-04-24 02:18:20 +0500

    more home page changes

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -36,7 +36,7 @@ class SecondPage extends StatelessWidget {
     return Scaffold(
         appBar: PreferredSize(
           preferredSize:
-              Size.fromHeight(100), // Adjust the height of the app bar
+              Size.fromHeight(65), // Adjust the height of the app bar
           child: AppBar(
             automaticallyImplyLeading: false,
             title: Padding(
@@ -45,7 +45,7 @@ class SecondPage extends StatelessWidget {
                 'Flex Student Portal',
                 style: TextStyle(
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w900,
                   fontSize: 30,
                 ),
               ),
@@ -82,15 +82,15 @@ class SecondPage extends StatelessWidget {
                         right: 0,
                         child: Container(
                           color: Colors.black,
-                          height: MediaQuery.of(context).size.height * 0.13,
+                          height: MediaQuery.of(context).size.height * 0.15,
                         ),
                       ),
                       Positioned(
-                        top: 15,
+                        top: 30,
                         left: 37,
                         child: Container(
-                          width: 318,
-                          height: 160,
+                          width: 325,
+                          height: 136,
                           decoration: BoxDecoration(
                             color: Color(0xFFE1F197),
                             borderRadius: BorderRadius.circular(25),
@@ -104,49 +104,180 @@ class SecondPage extends StatelessWidget {
                             ],
                           ),
                           child: Padding(
-                            padding: const EdgeInsets.all(12.0),
+                            padding: const EdgeInsets.all(15.0),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment
-                                  .center, // Center align the text
+                                  .start, // Center align the text
                               mainAxisAlignment: MainAxisAlignment.center,
 
                               children: [
                                 Text(
                                   'Name: ${items.isNotEmpty ? items[0]['name'] : 'No Name'}',
                                   style: TextStyle(
-                                      fontSize: 14,
+                                      fontSize: 12.5,
                                       fontWeight: FontWeight.w500),
                                 ),
-                                SizedBox(height: 5),
+                                SizedBox(height: 4),
                                 Text(
                                   'Blood Group: ${items.isNotEmpty ? items[0]['blood_group'] : 'No Blood Group'}',
                                   style: TextStyle(
-                                      fontSize: 14,
+                                      fontSize: 12.5,
                                       fontWeight: FontWeight.w500),
                                 ),
-                                SizedBox(height: 5),
+                                SizedBox(height: 4),
                                 Text(
                                   'CNIC: ${items.isNotEmpty ? items[0]['CNIC'] : 'No CNIC'}',
                                   style: TextStyle(
-                                      fontSize: 14,
+                                      fontSize: 12.5,
                                       fontWeight: FontWeight.w500),
                                 ),
-                                SizedBox(height: 5),
+                                SizedBox(height: 4),
                                 Text(
                                   'Email: ${items.isNotEmpty ? items[0]['email'] : 'No Email'}',
                                   style: TextStyle(
-                                      fontSize: 14,
+                                      fontSize: 12.5,
                                       fontWeight: FontWeight.w500),
                                 ),
-                                SizedBox(height: 5),
+                                SizedBox(height: 4),
                                 Text(
                                   'DOB: ${items.isNotEmpty ? (items[0]['DOB'] != null ? items[0]['DOB'].toDate().toString() : 'No DOB') : 'No DOB'}',
                                   style: TextStyle(
-                                      fontSize: 14,
+                                      fontSize: 12.5,
                                       fontWeight: FontWeight.w500),
                                 ),
                               ],
                             ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 225, // Adjust the position as needed
+                        left: 37,
+                        bottom: 0, // Extend to the bottom of the screen
+                        child: Container(
+                          width: 325,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 45),
+                              Container(
+                                width: 267,
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF30312C),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 27),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Personal Information',
+                                        style: TextStyle(
+                                            fontSize: 14.5,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Icon(Icons.arrow_circle_right_outlined,
+                                          color: Colors.white),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 22),
+                              Container(
+                                width: 267,
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF30312C),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 27),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Contact Information',
+                                        style: TextStyle(
+                                            fontSize: 14.5,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Icon(Icons.arrow_circle_right_outlined,
+                                          color: Colors.white),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 22),
+                              Container(
+                                width: 267,
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF30312C),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 27),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Family Information',
+                                        style: TextStyle(
+                                            fontSize: 14.5,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Icon(Icons.arrow_circle_right_outlined,
+                                          color: Colors.white),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 22),
+                              Container(
+                                width: 267,
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF30312C),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 27),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Academic Calender',
+                                        style: TextStyle(
+                                            fontSize: 14.5,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Icon(Icons.arrow_circle_right_outlined,
+                                          color: Colors.white),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
